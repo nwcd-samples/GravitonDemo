186ba2d48ec6bc96053fa08ab49adcbfee790d50
liyxmse <liyxmse@amazon.com>
2023-02-22 00:31:04 +0800

    update nodejs demo

--- graviton-demo-nodejs/nodejsdemo.js
+++ graviton-demo-nodejs/nodejsdemo.js
@@ -1,13 +1,31 @@
-var http = require('http');
+const http = require('http');
+const metadata = require('node-ec2-metadata');
+const Q = require('q');
 
+const host = '0.0.0.0';
+const port = 5000;
 
- 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+const requestListener = function (req, res) {
+    Q.all([
+        metadata.getMetadataForInstance('instance-id'),
+        metadata.getMetadataForInstance('instance-type'),
+        metadata.getMetadataForInstance('placement/availability-zone'),
+    ])
+    .spread(function(instanceId, instanceType, az) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("<html><body><h1>Graviton University Nodejs Demo</h1>");
+        res.write("Instance ID: " + instanceId + "<br>");
+        res.write("Instance Type: " + instanceType + "<br>");
+        res.write("Instance AZ: " + az + "<br>");
+        res.write("Node.js runtime: " + process.version + "<br>");
+        res.write("</body></html>");
+        res.end("");
+    })
 
-    response.write("<html><body><h1>Graviton University Nodejs Demo</h1>");
-    response.write("</body></html>");
-    response.end();
-).listen(5000);
+};
 
-// 
-console.log('Server running ...');
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
